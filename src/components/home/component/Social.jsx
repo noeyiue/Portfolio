45bdcba2e2df41eb7194f14de6edf0488bfd3020
PC <parat.kantida@gmail.com>
2022-09-30 23:36:00 +0700

    Finish Home-page

--- src/components/home/component/Social.jsx
+++ src/components/home/component/Social.jsx
@@ -4,15 +4,15 @@ const Social = () => {
   return (
     <div className="home_social">
 
-        <a href="https://www.instagram.com/" className="home_social-icon" target="_blank">
+        <a href="https://www.instagram.com/" className="home_social-icon" target="_blank" rel="noreferrer">
             <i className='uil uil-instagram'></i>
         </a>
 
-        <a href="https://dribbble.com/" className="home_social-icon" target="_blank">
+        <a href="https://dribbble.com/" className="home_social-icon" target="_blank" rel="noreferrer">
             <i className='uil uil-dribbble'></i>   
         </a>
 
-        <a href="https://github.com/noeyiue" className="home_social-icon" target="_blank">
+        <a href="https://github.com/noeyiue" className="home_social-icon" target="_blank" rel="noreferrer">
             <i className='uil uil-github-alt'></i>
         </a>
 
