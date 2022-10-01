3d34111a17c64ab6f0dd910a1a392a0b354bd6aa
PC <parat.kantida@gmail.com>
2022-10-01 23:44:07 +0700

    Edit Social-icon

--- src/components/home/home_component/Social.jsx
+++ src/components/home/home_component/Social.jsx
@@ -4,13 +4,13 @@ const Social = () => {
   return (
     <div className="home_social">
 
-        <a href="https://www.instagram.com/" className="home_social-icon" target="_blank" rel="noreferrer">
+        {/* <a href="https://www.instagram.com/" className="home_social-icon" target="_blank" rel="noreferrer">
             <i className='uil uil-instagram'></i>
         </a>
 
         <a href="https://dribbble.com/" className="home_social-icon" target="_blank" rel="noreferrer">
             <i className='uil uil-dribbble'></i>   
-        </a>
+        </a> */}
 
         <a href="https://github.com/noeyiue" className="home_social-icon" target="_blank" rel="noreferrer">
             <i className='uil uil-github-alt'></i>
